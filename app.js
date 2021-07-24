c0295a7a989ffacf851314cf6c965c5eec7aa9e1
sagar.thorat <sagar.thorat@cenitex.vic.gov.au>
2021-07-25 09:29:19 +1000

    added high and low score win options

--- app.js
+++ app.js
@@ -19,10 +19,11 @@ const app = Vue.createApp({
             id: 3,
             winningScore: 0,
             winner: '',
-            className: 'hide',
+            classNameHigh: 'hide',
             scoreToAdd: '',
             scoreToAdd1: '',
-            scoreToAdd2: ''
+            scoreToAdd2: '',
+            classNameLow: 'hide'
         }
     },
     methods: {
@@ -36,42 +37,63 @@ const app = Vue.createApp({
             } else {
                 alert('only numbers are allowed in score')
             }
-
-
-        },
-        scoreDown(player) {
-            if (player.score > 0) {
-                player.score -= 1;
-
-            }
         },
 
         addNames(item) {
             const regex = /[a-zA-Z]/;
             if (regex.test(item)) {
-                this.initName.push(item)
+                this.initName.push(item.toUpperCase())
+                if (this.id > 0) {
+                    this.id -= 1
+                }
             } else {
                 alert("Please enter valid name, no numbers or special characters!!!");
             }
+        },
+        finishGameLowScore() {
+            if (confirm('Are you sure you wish to end the game?')) {
 
 
-            if (this.id > 0) {
-                this.id -= 1
+                this.winningScore = Math.min(this.player1.score, this.player2.score, this.player3.score)
+                console.log(this.winningScore);
+                this.classNameLow = 'show';
+                this.classNameHigh = 'hide';
+                // winner.style.display = "block";
+
+                if (this.player1.score == this.winningScore) {
+                    this.winner = this.initName[0]
+                } else if (this.player2.score == this.winningScore) {
+                    this.winner = this.initName[1]
+                } else {
+                    this.winner = this.initName[2]
+                }
+
+            }
+
+        },
+        resetGame() {
+            if (alert('are you sure?')) {
+                this.player1.score = 0;
+                this.player2.score = 0;
+                this.player3.score = 0;
             }
         },
-        finishGame() {
+        finishGameHighScore() {
 
-            this.winningScore = Math.min(this.player1.score, this.player2.score, this.player3.score)
-            console.log(this.winningScore);
-            this.className = 'show'
-            // winner.style.display = "block";
+            if (confirm('Are you sure you wish to end the game?')) {
+                this.winningScore = Math.max(this.player1.score, this.player2.score, this.player3.score)
+                console.log(this.winningScore);
+                this.classNameHigh = 'show';
+                this.classNameLow = 'hide';
+                // winner.style.display = "block";
 
-            if (this.player1.score == this.winningScore) {
-                this.winner = this.initName[0]
-            } else if (this.player2.score == this.winningScore) {
-                this.winner = this.initName[1]
-            } else {
-                this.winner = this.initName[2]
+                if (this.player1.score == this.winningScore) {
+                    this.winner = this.initName[0]
+                } else if (this.player2.score == this.winningScore) {
+                    this.winner = this.initName[1]
+                } else {
+                    this.winner = this.initName[2]
+                }
             }
         }
     },
