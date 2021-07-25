12c7e5dcda989428e0db4d7930da1b8d02145496
arnavthorat78 <arnavthorat78@gmail.com>
2021-07-25 10:23:31 +1000

    Added Bootstrap styles

--- app.js
+++ app.js
@@ -1,119 +1,115 @@
 const app = Vue.createApp({
+	data() {
+		return {
+			player1: {
+				score: 0,
+			},
+			player2: {
+				score: 0,
+			},
+			player3: {
+				score: 0,
+			},
+			initName: [],
+			names: "",
+			id: 3,
+			winningScore: 0,
+			winner: "",
+			classNameHigh: "hide",
+			scoreToAdd: "",
+			scoreToAdd1: "",
+			scoreToAdd2: "",
+			classNameLow: "hide",
+		};
+	},
+	methods: {
+		scoreUp(player, num) {
+			const regex = /[1-9]/;
+			if (regex.test(num)) {
+				player.score += Number(num);
+				if (this.initName.length < 2) {
+					alert("Please add at least two players first!");
+				}
+			} else {
+				alert("Only numbers are allowed in the score.");
+			}
+		},
 
-    data() {
-        return {
-            player1: {
-                score: 0
+		addNames(item) {
+			const regex = /[a-zA-Z]/;
+			if (regex.test(item)) {
+				this.initName.push(item);
+				if (this.id > 0) {
+					this.id -= 1;
+				}
+			} else {
+				alert("Please enter a valid name (no special characters, numbers, or emojis).");
+			}
+		},
+		finishGameLowScore() {
+			if (confirm("Are you sure you want to end the game?")) {
+				this.winningScore = Math.min(
+					this.player1.score,
+					this.player2.score,
+					this.player3.score
+				);
+				console.log(this.winningScore);
+				this.classNameLow = "show";
+				this.classNameHigh = "hide";
+				// winner.style.display = "block";
 
-            },
-            player2: {
-                score: 0
+				if (this.player1.score == this.winningScore) {
+					this.winner = this.initName[0];
+				} else if (this.player2.score == this.winningScore) {
+					this.winner = this.initName[1];
+				} else {
+					this.winner = this.initName[2];
+				}
+			}
+		},
+		resetGame() {
+			if (alert("are you sure?")) {
+				this.player1.score = 0;
+				this.player2.score = 0;
+				this.player3.score = 0;
+			}
+		},
+		finishGameHighScore() {
+			if (confirm("Are you sure you want to end the game?")) {
+				this.winningScore = Math.max(
+					this.player1.score,
+					this.player2.score,
+					this.player3.score
+				);
+				console.log(this.winningScore);
+				this.classNameHigh = "show";
+				this.classNameLow = "hide";
+				// winner.style.display = "block";
 
-            },
-            player3: {
-                score: 0
+				if (this.player1.score == this.winningScore) {
+					this.winner = this.initName[0];
+				} else if (this.player2.score == this.winningScore) {
+					this.winner = this.initName[1];
+				} else {
+					this.winner = this.initName[2];
+				}
+			}
+		},
+	},
+	computed: {
+		getName() {
+			return this.initName[0];
+		},
+		getName1() {
+			return this.initName[1];
+		},
+		getName2() {
+			return this.initName[2];
+		},
+		disableButton() {
+			return this.id == 0;
+		},
+	},
+});
 
-            },
-            initName: [],
-            names: '',
-            id: 3,
-            winningScore: 0,
-            winner: '',
-            classNameHigh: 'hide',
-            scoreToAdd: '',
-            scoreToAdd1: '',
-            scoreToAdd2: '',
-            classNameLow: 'hide'
-        }
-    },
-    methods: {
-        scoreUp(player, num) {
-            const regex = /[1-9]/;
-            if (regex.test(num)) {
-                player.score += Number(num);
-                if (this.initName.length < 2) {
-                    alert('Please add atleast 2 players first!!!')
-                }
-            } else {
-                alert('only numbers are allowed in score')
-            }
-        },
-
-        addNames(item) {
-            const regex = /[a-zA-Z]/;
-            if (regex.test(item)) {
-                this.initName.push(item.toUpperCase())
-                if (this.id > 0) {
-                    this.id -= 1
-                }
-            } else {
-                alert("Please enter valid name, no numbers or special characters!!!");
-            }
-        },
-        finishGameLowScore() {
-            if (confirm('Are you sure you wish to end the game?')) {
-
-
-                this.winningScore = Math.min(this.player1.score, this.player2.score, this.player3.score)
-                console.log(this.winningScore);
-                this.classNameLow = 'show';
-                this.classNameHigh = 'hide';
-                // winner.style.display = "block";
-
-                if (this.player1.score == this.winningScore) {
-                    this.winner = this.initName[0]
-                } else if (this.player2.score == this.winningScore) {
-                    this.winner = this.initName[1]
-                } else {
-                    this.winner = this.initName[2]
-                }
-
-            }
-
-        },
-        resetGame() {
-            if (alert('are you sure?')) {
-                this.player1.score = 0;
-                this.player2.score = 0;
-                this.player3.score = 0;
-            }
-        },
-        finishGameHighScore() {
-
-            if (confirm('Are you sure you wish to end the game?')) {
-                this.winningScore = Math.max(this.player1.score, this.player2.score, this.player3.score)
-                console.log(this.winningScore);
-                this.classNameHigh = 'show';
-                this.classNameLow = 'hide';
-                // winner.style.display = "block";
-
-                if (this.player1.score == this.winningScore) {
-                    this.winner = this.initName[0]
-                } else if (this.player2.score == this.winningScore) {
-                    this.winner = this.initName[1]
-                } else {
-                    this.winner = this.initName[2]
-                }
-            }
-        }
-    },
-    computed: {
-        getName() {
-            return this.initName[0];
-        },
-        getName1() {
-            return this.initName[1];
-        },
-        getName2() {
-            return this.initName[2];
-        },
-        disableButton() {
-            return (this.id == 0)
-
-        }
-    }
-
-
-})
-
-app.mount('#assignment');
+app.mount("#assignment");
